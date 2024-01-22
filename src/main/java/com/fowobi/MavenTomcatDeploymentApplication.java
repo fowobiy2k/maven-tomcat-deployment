9d96391423ddc129981cf488373a7043f7935c62
oluwatosin <fowobiy2k@yahoo.com>
2024-01-22 15:23:24 +0100

    initial commit

---
+++ src/main/java/com/fowobi/MavenTomcatDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.fowobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTomcatDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenTomcatDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fowobi/controller/CoreController.java
@@ -0,0 +1,13 @@
+package com.fowobi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoreController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
